9337f16b04d80dbc67acb1a347185d585e0978d7
sfrunza <frunza.sergiu3@gmail.com>
2025-01-27 13:49:04 -0500

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
